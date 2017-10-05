721a67dd4119c31f11b9b62f39ffd6a42d72ff64
Jesse Plamondon-Willard <github@jplamondonw.com>
2017-10-05 15:34:48 -0400

    update for SMAPI 2.0 compatibility

--- PondWithBridge/PondWithBridge.cs
+++ PondWithBridge/PondWithBridge.cs
@@ -12,7 +12,9 @@ namespace PondWithBridge
 
         public static bool farmModified = false;
 
-        public override void Entry(params object[] objects)
+        /// <summary>The mod entry point, called after the mod is first loaded.</summary>
+        /// <param name="helper">Provides simplified APIs for writing mods.</param>
+        public override void Entry(IModHelper helper)
         {
             GameEvents.SecondUpdateTick += Event_SecondUpdateTick;
         }
