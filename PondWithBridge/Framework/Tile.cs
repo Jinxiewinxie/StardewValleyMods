edd08158ec1693390f3ecff0210f84fde67a4adf
Jesse Plamondon-Willard <Pathoschild@users.noreply.github.com>
2019-07-11 20:06:22 -0400

    move class into its own file

---
+++ PondWithBridge/Framework/Tile.cs
@@ -0,0 +1,53 @@
+﻿namespace PondWithBridge.Framework
+{
+    public class Tile
+    {
+        public int l;
+        public int x;
+        public int y;
+        public int tileIndex;
+        public string layer;
+        public int tileSheet = 1;
+
+        public Tile(int l, int x, int y, int tileIndex)
+        {
+            this.l = l;
+            this.x = x;
+            this.y = y;
+            this.tileIndex = tileIndex;
+            setLayerName(l);
+        }
+
+        public Tile(int l, int x, int y, int tileIndex, int tileSheet)
+        {
+            this.l = l;
+            this.x = x;
+            this.y = y;
+            this.tileIndex = tileIndex;
+            this.tileSheet = tileSheet;
+            setLayerName(l);
+        }
+
+        public void setLayerName(int l)
+        {
+            switch (l)
+            {
+                case 0:
+                    this.layer = "Back";
+                    break;
+                case 1:
+                    this.layer = "Buildings";
+                    break;
+                case 2:
+                    this.layer = "Paths";
+                    break;
+                case 3:
+                    this.layer = "Front";
+                    break;
+                case 4:
+                    this.layer = "AlwaysFront";
+                    break;
+            }
+        }
+    }
+}
--- PondWithBridge/ModEntry.cs
+++ PondWithBridge/ModEntry.cs
@@ -3,6 +3,7 @@ using StardewModdingAPI;
 using StardewModdingAPI.Events;
 using StardewValley;
 using xTile.Tiles;
+using Tile = PondWithBridge.Framework.Tile;
 
 namespace PondWithBridge
 {
@@ -90,50 +91,6 @@ namespace PondWithBridge
                 {
                     gl.setMapTileIndex(tile.x, tile.y, tile.tileIndex, tile.layer);
                 }
-            }
-        }
-    }
-
-    public class Tile
-    {
-        public int l;
-        public int x;
-        public int y;
-        public int tileIndex;
-        public string layer;
-        public int tileSheet = 1;
-
-        public Tile(int l, int x, int y, int tileIndex)
-        {
-            this.l = l; this.x = x; this.y = y; this.tileIndex = tileIndex;
-            setLayerName(l);
-        }
-
-        public Tile(int l, int x, int y, int tileIndex, int tileSheet)
-        {
-            this.l = l; this.x = x; this.y = y; this.tileIndex = tileIndex; this.tileSheet = tileSheet;
-            setLayerName(l);
-        }
-
-        public void setLayerName(int l)
-        {
-            switch (l)
-            {
-                case 0:
-                    this.layer = "Back";
-                    break;
-                case 1:
-                    this.layer = "Buildings";
-                    break;
-                case 2:
-                    this.layer = "Paths";
-                    break;
-                case 3:
-                    this.layer = "Front";
-                    break;
-                case 4:
-                    this.layer = "AlwaysFront";
-                    break;
             }
         }
     }
